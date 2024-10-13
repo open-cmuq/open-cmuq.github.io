ef0edb4fa4fd8d564404ddc9781deb8c9475004a
Mateo <me@matc.dev>
2024-10-13 10:58:06 -0700

    Update astro.config.mjs
    Update site and base settings for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: 'https://open-cmuq.github.io',
+  base: '/',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
